52116ccb2925e0ff77411f93a2d52cd68bf30b95
abdinasir <abdinasiradan083@gmail.com>
2023-01-27 11:10:53 +0300

    square task

---
+++ tasks/square.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main(){
+    int square , value;
+    printf("enter a number \n");
+    scanf("%i",&value);
+    square = value * value;
+    printf(" the square of %i is %i \n", value, square);
+
+}
