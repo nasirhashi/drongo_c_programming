693ecb730795094fe018b4c8c31f005822f5d6fa
abdinasir <abdinasiradan083@gmail.com>
2023-02-02 10:45:26 +0300

    if else if statement

---
+++ 02_conditions/con03.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+    int x = 3;
+    if (x > 3)
+    {
+        printf("x is is greater than 3\n");
+    }
+    else if (x < 3)
+    {
+        printf("x is less than 3\n");
+    }
+    else
+    {
+        printf("x is 3\n");
+    }
+}
+// if else if statement 
